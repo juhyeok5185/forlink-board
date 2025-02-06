311f50cdf4d3b8b9825a5bc27795da5ce047cc71
juhyeok5185 <qkrwngur5185@naver.com>
2025-02-06 15:15:41 +0900

    added

--- server.js
+++ server.js
@@ -2,6 +2,4 @@ const app = require("./src/app"); // Express 설정 불러오기
 
 const port = 3000;
 
-app.listen(port, () => {
-    console.log(`🚀 서버 실행 중: http://localhost:${port}`);
-});
+app.listen(port, () => {console.log(`🚀 서버 실행 중: http://localhost:${port}`);});
